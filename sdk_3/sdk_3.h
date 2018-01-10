76f9963e59188336ebe49af577ef5bf3f4652e50
zanmato <ruoxi.sun@transwarp.io>
2018-01-10 19:47:40 +0800

    Remove duplicate empty line.

--- sdk_3/sdk_3.h
+++ sdk_3/sdk_3.h
@@ -9,7 +9,6 @@ extern get_sth_f_type get_sth;
 typedef void (*set_sth_f_type)(const std::string&);
 extern set_sth_f_type set_sth;
 
-
 typedef void (*__init_f_type)(get_sth_f_type, set_sth_f_type);
 extern "C" void __init(get_sth_f_type, set_sth_f_type);
 
